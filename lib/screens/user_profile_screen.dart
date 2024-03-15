438aaff9703ece733103147a30476692d06a09f7
nejawittatchy <nejawittatchy@gmail.com>
2024-03-15 16:57:52 +0530

    Add Interfaces of chat screen and user profile

--- lib/screens/user_profile_screen.dart
+++ lib/screens/user_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisewave/components/theme/main_bg_gradient.dart';
+import 'package:wisewave/components/theme/nav_bg_gradient.dart';
 
 class UserProfileScreen extends StatelessWidget {
   const UserProfileScreen({super.key});
@@ -7,18 +8,121 @@ class UserProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Profile',
+          style: TextStyle(
+            fontWeight: FontWeight.bold, 
+            fontSize: 24.0,
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: setNavBgGradient(),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Navigate back to the previous screen
+          },
+        ),
+      ),
       body: Container(
         decoration: setMainBgGradient(),
-        child: const Center(
-          child: Text(
-            "User Profile screen",
-            style: TextStyle(
-              fontSize: 33,
-              fontFamily: 'Epilogue',
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 55, 55, 55),
+        padding: EdgeInsets.all(50.0),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                CircleAvatar(
+                  radius: 80.0,
+                  backgroundImage: AssetImage('assets/profile_picture.png'), // replace image
+                  
+                ),
+                
+                Opacity(
+                  opacity: 0.5,
+                  child: Icon(
+                    Icons.camera, // change icon
+                    color: Colors.grey,
+                    size: 60.0,
+                  ),
+                ),
+                ],
+                ),
+                SizedBox(height: 20.0),
+                Center(
+                  child: Text(
+                    'Sarina',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
+            SizedBox(height: 50.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.account_circle),
+                      SizedBox(width: 10.0),
+                      Text('Account details', style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.settings),
+                      SizedBox(width: 10.0),
+                      Text('Settings', 
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.logout),
+                      SizedBox(width: 10.0),
+                      Text('Log out', style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
