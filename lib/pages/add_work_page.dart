fa74f747aca6e5e185d25aab88298339e6e50aad
IsuruDinuranga <isurudinuranga08@gmail.com>
2024-03-23 02:46:22 +0530

    Add work load screen and work load page and also add a button to home screen to navigate the workload screen

---
+++ lib/pages/add_work_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddWorkPage extends StatelessWidget {
+  const AddWorkPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Add Work Page'),
+      ),
+    );
+  }
+}
