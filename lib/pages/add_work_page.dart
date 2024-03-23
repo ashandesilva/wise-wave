3528422e133ee22ec8c4f87ef8900d9295b53e9f
NJ-PRO <naaji50@gmail.com>
2024-03-24 04:24:24 +0530

    added new workload to the adding FAB, and create the UI for workload...

--- lib/pages/add_work_page.dart
+++ lib/pages/add_work_page.dart
@@ -12,190 +12,199 @@ class AddWorkPage extends StatefulWidget {
 }
 
 class _AddWorkPageState extends State<AddWorkPage> {
- late  DateTime _focusedDay;
- DateTime? _selectDay;
+  late DateTime _focusedDay;
+  DateTime? _selectDay;
 
   @override
   void initState() {
     super.initState();
     _focusedDay = DateTime.now();
-     // Initialize _focusedDay to the current date
+    // Initialize _focusedDay to the current date
   }
-   String _bottmButtonText = "Add My Work";
-  Widget _leadingIcon = const Text('');
-  bool _isBottomButtonPressed = false;
-  final bool _isSliderChanged = false;
+
+  final String _bottmButtonText = "Add My Work";
   final TextEditingController _titleTextFieldController =
       TextEditingController();
   final TextEditingController _addDetailsTextFieldController =
       TextEditingController();
 
-  Widget _workLoadBody() {
-    return Container(
-      decoration: setMainBgGradient(),
-      child: Center(
-        child: Container(
-          margin: const EdgeInsets.fromLTRB(0, 107, 0, 0),
-          padding: const EdgeInsets.only(bottom: 100),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TableCalendar(
-                firstDay: DateTime.utc(2010, 10, 16),
-                lastDay: DateTime.utc(2030, 3, 14),
-                focusedDay: _focusedDay,
-                selectedDayPredicate: (day) {
-                  return isSameDay(_selectDay, day);
-                },
-                onDaySelected: (selectedDay, focusedDay) {
-                  setState(() {
-                    _selectDay = selectedDay;
-                    _focusedDay = _focusedDay;
-                  });
-                },
-                onPageChanged: (focusedDay){
-                  _focusedDay = focusedDay;
-                },
-                calendarStyle: CalendarStyle(
-    // Use `CalendarStyle` to customize the calendar's appearance
-                  outsideDaysVisible: true,
-                  defaultTextStyle: const TextStyle(fontWeight: FontWeight.bold),
-                  weekendTextStyle: const TextStyle().copyWith(color: Colors.red[800]),
-                  holidayTextStyle: const TextStyle().copyWith(color: Colors.green[800]),
-                ),
-                daysOfWeekStyle: DaysOfWeekStyle(
-                  // Use `DaysOfWeekStyle` to customize the appearance of the days of the week
-                  weekendStyle: const TextStyle().copyWith(color: Colors.red[600]),
-                ),
-                headerStyle: HeaderStyle(
-                  // Use `HeaderStyle` to customize the appearance of the calendar's header
-                  formatButtonVisible: false,
-                  titleCentered: true,
-                  formatButtonShowsNext: true,
-                  formatButtonDecoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  formatButtonTextStyle: const TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-                const SizedBox(height: 20),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 30),
-                  child: TextField(
-                    controller: _titleTextFieldController,
-                    onTap: () async => setState(() {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => TitleTextfieldScreen(
-                            initialValue: _titleTextFieldController.text,
-                          ),
-                        ),
-                      ).then((enteredValue) {
-                        if (enteredValue != null) {
-                          setState(() {
-                            _titleTextFieldController.text = enteredValue;
-                          });
-                        }
-                      });
-                    }),
-                    autofocus: false,
-                    readOnly: true,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: const Color(0xFFE3F4F7),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Color(0xFFE3F4F7)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Color(0xFFE3F4F7)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      hintText: 'Title...',
-                      hintStyle: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(122, 71, 71, 71),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 30),
-                  child: TextField(
-                    controller: _addDetailsTextFieldController,
-                    onTap: () async => setState(() {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AddNotesTextfieldScreen(
-                            initialValue: _addDetailsTextFieldController.text,
-                          ),
-                        ),
-                      ).then((enteredValue) {
-                        if (enteredValue != null) {
-                          setState(() {
-                            _addDetailsTextFieldController.text = enteredValue;
-                          });
-                        }
-                      });
-                    }),
-                    maxLines: 3,
-                    autofocus: false,
-                    readOnly: true,
-                    style: const TextStyle(fontSize: 15),
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: const Color(0xFFE3F4F7),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Color(0xFFE3F4F7)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Color(0xFFE3F4F7)),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      hintText: 'Add Details of the Work...',
-                      hintStyle: const TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(122, 71, 71, 71),
-                      ),
-                    ),
-                  ),
-                ),
-              ]
-            ),
-          )
-        ),
-      )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _myAppBar(context),
+      body: _workLoadBody(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _bottomButton(context),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-     Widget checkInBody =
-        !_isBottomButtonPressed ? _workLoadBody() : _workLoadBody();
-    return Scaffold(
-      extendBody: true,
-      extendBodyBehindAppBar: true,
-      appBar: _myAppBar(context),
-      body: checkInBody,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: _bottomButton(context),
-     
+  Widget _workLoadBody() {
+    return Container(
+      decoration: setMainBgGradient(),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                  color: Color(0xFFE3F4F7),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 10.0,
+                ),
+                child: TableCalendar(
+                  firstDay: DateTime.utc(2010, 10, 16),
+                  lastDay: DateTime.utc(2030, 3, 14),
+                  focusedDay: _focusedDay,
+                  selectedDayPredicate: (day) {
+                    return isSameDay(_selectDay, day);
+                  },
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectDay = selectedDay;
+                      _focusedDay = _focusedDay;
+                    });
+                  },
+                  onPageChanged: (focusedDay) {
+                    _focusedDay = focusedDay;
+                  },
+                  calendarStyle: CalendarStyle(
+                    // Use `CalendarStyle` to customize the calendar's appearance
+                    outsideDaysVisible: true,
+                    defaultTextStyle:
+                        const TextStyle(fontWeight: FontWeight.bold),
+                    weekendTextStyle:
+                        const TextStyle().copyWith(color: Colors.red[800]),
+                    holidayTextStyle:
+                        const TextStyle().copyWith(color: Colors.green[800]),
+                  ),
+                  daysOfWeekStyle: DaysOfWeekStyle(
+                    // Use `DaysOfWeekStyle` to customize the appearance of the days of the week
+                    weekendStyle:
+                        const TextStyle().copyWith(color: Colors.red[600]),
+                  ),
+                  headerStyle: HeaderStyle(
+                    // Use `HeaderStyle` to customize the appearance of the calendar's header
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                    formatButtonShowsNext: true,
+                    formatButtonDecoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    formatButtonTextStyle: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextField(
+                  controller: _titleTextFieldController,
+                  onTap: () async => setState(() {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => TitleTextfieldScreen(
+                          initialValue: _titleTextFieldController.text,
+                        ),
+                      ),
+                    ).then((enteredValue) {
+                      if (enteredValue != null) {
+                        setState(() {
+                          _titleTextFieldController.text = enteredValue;
+                        });
+                      }
+                    });
+                  }),
+                  autofocus: false,
+                  readOnly: true,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFFE3F4F7),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xFFE3F4F7)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xFFE3F4F7)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'Title...',
+                    hintStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(122, 71, 71, 71),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextField(
+                  controller: _addDetailsTextFieldController,
+                  onTap: () async => setState(() {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AddNotesTextfieldScreen(
+                          initialValue: _addDetailsTextFieldController.text,
+                        ),
+                      ),
+                    ).then((enteredValue) {
+                      if (enteredValue != null) {
+                        setState(() {
+                          _addDetailsTextFieldController.text = enteredValue;
+                        });
+                      }
+                    });
+                  }),
+                  maxLines: 3,
+                  autofocus: false,
+                  readOnly: true,
+                  style: const TextStyle(fontSize: 15),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFFE3F4F7),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xFFE3F4F7)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Color(0xFFE3F4F7)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'Add Details of the Work...',
+                    hintStyle: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(122, 71, 71, 71),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
+
   Padding _bottomButton(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
@@ -218,19 +227,10 @@ class _AddWorkPageState extends State<AddWorkPage> {
             ),
           ),
           FilledButton(
-            onPressed: _isSliderChanged
-                ? () {
-                    setState(() {
-                      if (!_isBottomButtonPressed) {
-                        _bottmButtonText = "Add Work";
-                        _isBottomButtonPressed = true;
-                        _leadingIcon = _getLeadingBackIcon();
-                      } else {
-                        Navigator.pop(context);
-                      }
-                    });
-                  }
-                : null,
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.pop(context);
+            },
             style: const ButtonStyle(
               fixedSize: MaterialStatePropertyAll(Size(250, 60)),
               backgroundColor: MaterialStatePropertyAll(Colors.transparent),
@@ -254,19 +254,39 @@ class _AddWorkPageState extends State<AddWorkPage> {
       ),
     );
   }
-  
+
   AppBar _myAppBar(BuildContext context) {
     return AppBar(
       systemOverlayStyle: const SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
         statusBarIconBrightness: Brightness.dark,
       ),
-      leadingWidth: 65,
       automaticallyImplyLeading: false,
-      scrolledUnderElevation: 10,
-      surfaceTintColor: const Color.fromARGB(255, 255, 255, 255),
+      title: const Padding(
+        padding: EdgeInsets.only(left: 10.0),
+        child: Text(
+          "Workload",
+          style: TextStyle(
+            color: Color(0xFF373737),
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+          ),
+        ),
+      ),
       backgroundColor: Colors.transparent,
-      leading: _leadingIcon,
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 242, 195, 178),
+              Color.fromARGB(255, 229, 168, 182)
+            ],
+            stops: [0, 1],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+      ),
       actions: <Widget>[
         GestureDetector(
           onTap: () async {
@@ -274,7 +294,7 @@ class _AddWorkPageState extends State<AddWorkPage> {
           },
           child: const Image(
             width: 45,
-            image: AssetImage("assets/images/close-button.png"),
+            image: AssetImage("assets/images/back-button.png"),
           ),
         ),
         const SizedBox(width: 20),
@@ -285,26 +305,4 @@ class _AddWorkPageState extends State<AddWorkPage> {
       ),
     );
   }
-  Row _getLeadingBackIcon() {
-    return Row(
-      children: [
-        const SizedBox(width: 20),
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              if (_isBottomButtonPressed) {
-                _bottmButtonText = "Add My Work";
-                _isBottomButtonPressed = false;
-                _leadingIcon = const Text('');
-              }
-            });
-          },
-          child: const Image(
-            width: 45,
-            image: AssetImage("assets/images/back-button.png"),
-          ),
-        ),
-      ],
-    );
-  } 
 }
--- lib/screens/work_load_screen.dart
+++ lib/screens/work_load_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wisewave/components/theme/main_bg_gradient.dart';
 import 'package:wisewave/pages/add_work_page.dart';
 
@@ -8,58 +9,128 @@ class WorkLoadScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _myAppBar(context),
+      floatingActionButton: _bottomButton(context),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: Container(
-          decoration: setMainBgGradient(),
-          child: ListView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
+        decoration: setMainBgGradient(),
+        child: ListView(
+          children: [
+            const SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Container(height: 100,
-                color: const Color.fromARGB(255, 200, 114, 207),),
-              ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    height: 100,
-                    color: const Color.fromARGB(255, 200, 114, 207),
-                  ),
+                borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  height: 100,
+                  color: const Color(0xFFE3F4F7),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    height: 100,
-                    color: const Color.fromARGB(255, 200, 114, 207),
-                  ),
-                ),
-              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const AddWorkPage()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  ),
-                  child: const Text('Add Work', style: TextStyle(fontSize: 20),),
-                )
-              )
-            ],
+  AppBar _myAppBar(BuildContext context) {
+    return AppBar(
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      automaticallyImplyLeading: false,
+      title: const Padding(
+        padding: EdgeInsets.only(left: 10.0),
+        child: Text(
+          "Workload",
+          style: TextStyle(
+            color: Color(0xFF373737),
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
           ),
         ),
+      ),
+      backgroundColor: Colors.transparent,
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 242, 195, 178),
+              Color.fromARGB(255, 229, 168, 182)
+            ],
+            stops: [0, 1],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        GestureDetector(
+          onTap: () async {
+            Navigator.pop(context);
+          },
+          child: const Image(
+            width: 45,
+            image: AssetImage("assets/images/close-button.png"),
+          ),
+        ),
+        const SizedBox(width: 20),
+      ],
+      bottom: const PreferredSize(
+        preferredSize: Size(0, 10),
+        child: SizedBox(height: 0),
+      ),
+    );
+  }
+
+  Padding _bottomButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 242, 195, 178),
+                    Color.fromARGB(255, 229, 168, 182)
+                  ],
+                  stops: [0, 1],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+              ),
+            ),
+          ),
+          FilledButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const AddWorkPage()));
+            },
+            style: const ButtonStyle(
+              fixedSize: MaterialStatePropertyAll(Size(250, 60)),
+              backgroundColor: MaterialStatePropertyAll(Colors.transparent),
+            ),
+            child: const Text(
+              "Add Work",
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 18,
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(0, 1.5),
+                    color: Color.fromARGB(38, 0, 0, 0),
+                    blurRadius: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
