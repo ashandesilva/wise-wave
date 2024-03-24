9ba1789ff4245bef430747ef2b1c75e3e5301272
Ashan De Silva <ashansdesilva@gmail.com>
2024-03-24 20:22:04 +0530

    Add "Say thanks" Backend call to SayThanksPage

--- lib/pages/say_thanks.dart
+++ lib/pages/say_thanks.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SayThanksPage extends StatelessWidget {
 
   //passed uid from nav_page.dart
   final String uid;
-  const SayThanksPage({super.key, required this.uid});
+  final _gratitudeController = TextEditingController();
+  SayThanksPage({Key? key, required this.uid}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +24,7 @@ class SayThanksPage extends StatelessWidget {
             ),
             SizedBox(height: 16),
             TextField(
+              controller: _gratitudeController,
               decoration: InputDecoration(
                 hintText: 'Today I am grateful for...',
               ),
@@ -29,7 +32,16 @@ class SayThanksPage extends StatelessWidget {
             SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                // TODO: Implement say thanks functionality
+                FirebaseFirestore.instance.collection('gratitude').add({
+                  'userId': uid,
+                  'date': DateTime.now(),
+                  'gratitude': _gratitudeController.text,
+                  }).then((_) {
+                    Navigator.pop(context);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Always remember to be thankful ❤️')),
+                    );
+                });
               },
               child: Text('Say thanks'),
             ),
