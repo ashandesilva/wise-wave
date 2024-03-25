72f3b657412dc94cadf99b3cfd1e088bfd20ce8e
NJ-PRO <naaji50@gmail.com>
2024-03-25 07:54:51 +0530

    minor UI changes

--- lib/pages/nav_page.dart
+++ lib/pages/nav_page.dart
@@ -322,13 +322,13 @@ class _NavPageState extends State<NavPage> {
         smallSize: 19,
         backgroundColor: Color(0xFF39B29C),
         child: Iconify(
-          Bxs.layer,
+          Bxs.analyse,
           size: 30,
           color: Color(0xFF39B29C),
         ),
       ),
       icon: Iconify(
-        Bx.layer,
+        Bx.analyse,
         size: 30,
         color: Color(0xFF472732),
       ),
